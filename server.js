c28e04d0ad2bf90c8d7d0ba95d2407686734f7c7
shchus <44160244+shchus@users.noreply.github.com>
2018-10-22 17:52:36 +0300

    done

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer  = require('multer')
+var upload = multer()
 
 var app = express();
 
@@ -14,8 +16,12 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res){
+  res.json({name: req.file.originalname,
+            type: req.file.mimetype,
+            size: req.file.size
+           });
+  console.log(req.file)
 });
 
 app.listen(process.env.PORT || 3000, function () {
